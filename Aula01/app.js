5d5189857d0df017bfc86414e1382f7bed41b653
“Marcus” <mvfbarbos@msn.com>
2019-08-08 18:55:44 -0400

    Validando cpf

--- Aula01/app.js
+++ Aula01/app.js
@@ -19,11 +19,27 @@ app.use("/inverter/:string",(req,res) => {
 
 
 //validando cpf
-app.use("/cpf/:cpf",(req,res) => {
-    let cpf =req.params.cpf;
+app.use('/cpf/:cpf', (req, res) => { 
+    let cpf = req.params.cpf;
+    let soma = 0;
+    let mod;
+    soma = 0;
+    //cpf com valores iguais 
+  if (cpf == "00000000000") res.send(false);
 
-    // Deixo pra vcs!
-    res.send("Validador de CPF");
+  for (i=1; i<=9; i++) soma = soma + parseInt(cpf.substring(i-1, i)) * (11 - i);
+  mod = (soma * 10) % 11;
+   
+    if ((mod == 10) || (mod == 11))  mod = 0;
+    if (mod != parseInt(cpf.substring(9, 10)) ) res.send(false);
+   
+    soma = 0;
+    for (i = 1; i <= 10; i++) soma = soma + parseInt(cpf.substring(i-1, i)) * (12 - i);
+    mod = (soma * 10) % 11;
+   
+    if ((mod == 10) || (mod == 11))  mod = 0;
+    if (mod != parseInt(cpf.substring(10, 11) ) ) res.send(false);
+    res.send(true);
 });
 
 
